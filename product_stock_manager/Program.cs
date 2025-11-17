de5faf71d4472f5b7b097e846509be491aa9b714
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2025-11-17 16:16:34 -0300

    Primeiro commit

---
+++ product_stock_manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace product_stock_manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
