b396e6811dda0cb292574c10b2e59647fef376e5
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2025-11-18 16:11:30 -0300

    Adicionando construtor da classe Seller

--- product_stock_manager/Entities/Seller.cs
+++ product_stock_manager/Entities/Seller.cs
@@ -5,5 +5,12 @@
         public int SellerId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+
+        public Seller(int sellerId, string name, string email)
+        {
+            SellerId = sellerId;
+            Name = name;
+            Email = email;
+        }
     }
 }
